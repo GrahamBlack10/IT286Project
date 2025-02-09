431fc7889556bb90a0ddbdfcce1de4c1a8f76ff0
Gurdeep2005 <gs652@njit.edu>
2025-02-09 16:24:06 -0500

    Added player animations
    also fixed jumping form happening mid air

--- Shadow Keep/Assets/PlayerMovementScript.cs
+++ Shadow Keep/Assets/PlayerMovementScript.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,10 +6,16 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public float horizontalMovementSpeed = 10;
     public float jumpHeight = 10;
-    private bool jumping = false;
+    public bool isAttacking = false;
+    public double attackBuffer = 0.05;
+    private bool isGrounded = true;
+    private string attackType = "closeRangeAttack";
+    private float attackTimer = 0;
     private string directionFacing = "right";
+    private double closeRangeAttackAnimationLength = 0.45;
     public Rigidbody2D myRidgidBody;
     public SpriteRenderer mySprite;
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,22 +26,47 @@ public class PlayerMovementScript : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.A)){
-            transform.position = new Vector3(transform.position.x - horizontalMovementSpeed*Time.deltaTime, transform.position.y, transform.position.z);
-            if(directionFacing == "right"){
+            myRidgidBody.linearVelocityX = -horizontalMovementSpeed;
+            if(directionFacing == "right" && !isAttacking){
                 mySprite.flipX = true;
                 directionFacing = "left";
             }
-        }
-        if(Input.GetKey(KeyCode.D)){
-            transform.position = new Vector3(transform.position.x + horizontalMovementSpeed*Time.deltaTime, transform.position.y, transform.position.z);
-            if(directionFacing == "left"){
+        }else if(Input.GetKey(KeyCode.D)){
+            myRidgidBody.linearVelocityX = horizontalMovementSpeed;
+            if(directionFacing == "left"  && !isAttacking){
                 mySprite.flipX = false;
                 directionFacing = "right";
             }
+        }else{
+            myRidgidBody.linearVelocityX = 0;
         }
-        if(Input.GetKeyDown(KeyCode.W) && !jumping){
+        if(Input.GetKeyDown(KeyCode.W) && isGrounded){
             myRidgidBody.linearVelocityY = jumpHeight;
-            //jumping = true;
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
         }
+
+        if(isAttacking){
+            attackTimer += Time.deltaTime;
+            if(attackType=="closeRangeAttack"){
+                animator.SetBool("closeRangeAttacking", false);
+                if(attackTimer >= closeRangeAttackAnimationLength + attackBuffer){
+                    isAttacking = false;
+                    attackTimer = 0;
+                }
+            }
+        }
+        if(Input.GetMouseButtonDown(0) && !isAttacking && isGrounded){
+            isAttacking = true;
+            animator.SetBool("closeRangeAttacking", true);  
+            attackType = "closeRangeAttack";
+        }
+        animator.SetFloat("xVelocity", math.abs(myRidgidBody.linearVelocityX));
+        animator.SetFloat("yVelocity", myRidgidBody.linearVelocityY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
     }
 }
